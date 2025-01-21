835c14b92688794783dd7dc0918ff3f0d62feeee
Mohammad Rafiq <googaabumtum@gmail.com>
2025-01-21 19:39:30 +0800

    Modified main.rs to use string literals instead of depending on the html files

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,15 +1,13 @@
 use std::{
-    fs,
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
 };
 
 fn main() {
     // Listen for connections on 7878
-    let listener = TcpListener::bind("0.0.0.0:7878").unwrap();
+    let listener = TcpListener::bind("0.0.0.0:7878").expect("Failed to bind");
     for stream in listener.incoming() {
-        let stream = stream.unwrap();
-
+        let stream = stream.expect("Failed to accept connection");
         handle_connection(stream);
     }
 }
@@ -18,17 +16,16 @@ fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    // Check for URI
-    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
-        ("HTTP/1.1 200 OK", "hello.html")
+    // Determine which file to serve
+    let (status_line, contents) = if request_line == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", include_str!("../assets/hello.html"))
     } else {
-        ("HTTP/1.1 404 NOT FOUND", "error.html")
+        ("HTTP/1.1 404 NOT FOUND", include_str!("../assets/error.html"))
     };
 
-    let contents = fs::read_to_string(filename).unwrap();
+    // Read the contents of the file
     let length = contents.len();
     let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-    
-    stream.write_all(response.as_bytes()).unwrap();
 
+    stream.write_all(response.as_bytes()).expect("Failed to write response");
 }
